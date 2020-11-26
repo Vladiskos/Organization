7ac72c783de59b7d1ffdb4eda3b5f5cfcf32c076
Vladislav Cehanovich <vlad03122000@gmail.com>
2020-11-26 23:11:49 +0300

    DAO impl for objects

---
+++ Online_store/src/com/java/automation/lab/fall/cehanovich/core22/domain/service/BaseService.java
@@ -0,0 +1,11 @@
+package com.java.automation.lab.fall.cehanovich.core22.domain.service;
+
+import com.java.automation.lab.fall.cehanovich.core22.domain.dao.AddressDAO;
+import com.java.automation.lab.fall.cehanovich.core22.domain.io.PropertyReader;
+
+import java.util.Map;
+
+public class BaseService {
+    protected static final PropertyReader PROPS = PropertyReader.getInstance();
+
+}
